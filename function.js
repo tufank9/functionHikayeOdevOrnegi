724631986509eadecbb88aa9915a074846e01489
tufan <tufankorkmazel3@gmail.com>
2023-12-18 02:01:02 +0300

    sorgu fonksiyonunda eğer devam yazarsa kullanıcı devam adında bir fonksiyon oluşturdum.

--- function.js
+++ function.js
@@ -9,4 +9,12 @@
     if (kelime(cevap)=="son") {
         return son()
     }
+    else if(kelime(cevap)=="devam"){
+        return devam() 
+    }
  }
+ function son(){
+    alert("hikayenin ilk sonu")
+    return alternatifSon1()
+ }
+ 
